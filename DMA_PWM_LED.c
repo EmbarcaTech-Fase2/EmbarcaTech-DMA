9c1e33e706204f41057ad20bf4c168cdd78c7403
Leonam Rabelo <rabeloleonam@gmail.com>
2025-12-08 00:10:12 -0300

    Ajustes e comentários no codigo

--- DMA_PWM_LED.c
+++ DMA_PWM_LED.c
@@ -4,43 +4,49 @@
 #include "hardware/dma.h"
 #include <stdio.h>
 
-#define LED_PIN 8
+// Definições de pinos
+#define SERVO_PIN 8
 #define PWM_WRAP 256
 
+// Buffer de fade
 static uint16_t fade[PWM_WRAP];
 static int dma_chan;
 ssd1306_t ssd;
 
-void dma_handler() {
-    dma_hw->ints0 = 1u << dma_chan;
-    dma_channel_set_read_addr(dma_chan, fade, true);
+// Função de tratamento de DMA
+void dma_handler(){
+    dma_hw->ints0 = 1u << dma_chan; // Limpa a interrupção
+    dma_channel_set_read_addr(dma_chan, fade, true);    // Configura o endereço de leitura
 }
 
 
 int main(){
-
+    // Inicializações
     stdio_init_all();
     display_init(&ssd);
-    gpio_set_function(LED_PIN, GPIO_FUNC_PWM);
-    uint slice_num = pwm_gpio_to_slice_num(LED_PIN);
-
+    gpio_set_function(SERVO_PIN, GPIO_FUNC_PWM);
+    uint slice_num = pwm_gpio_to_slice_num(SERVO_PIN);
+    
+    // Configuração do PWM
     pwm_config config = pwm_get_default_config();
     pwm_config_set_clkdiv(&config, 16.f);
     pwm_init(slice_num, &config, true);
 
-    for (int i = 0; i < PWM_WRAP/2; i++) {
+    // Configuração do buffer de fade
+    for(int i = 0; i < PWM_WRAP/2; i++){
         fade[i] = (uint16_t)((i * i *2));
         fade[PWM_WRAP - 1 - i] = fade[i];
-
     }
 
-    dma_chan = dma_claim_unused_channel(true);
-    dma_channel_config c = dma_channel_get_default_config(dma_chan);
-    channel_config_set_transfer_data_size(&c, DMA_SIZE_16);
-    channel_config_set_read_increment(&c, true);
-    channel_config_set_write_increment(&c, false);
-    channel_config_set_dreq(&c, DREQ_PWM_WRAP0 + slice_num);
+    // Configuração do DMA
+    dma_chan = dma_claim_unused_channel(true);  // Atribui um canal de DMA livre
+    dma_channel_config c = dma_channel_get_default_config(dma_chan);    // Configuração padrão do DMA
+    channel_config_set_transfer_data_size(&c, DMA_SIZE_16);             // Tamanho dos dados: 16 bits
+    channel_config_set_read_increment(&c, true);                        // Incrementa o endereço de leitura
+    channel_config_set_write_increment(&c, false);                       // Não incrementa o endereço de escrita
+    channel_config_set_dreq(&c, DREQ_PWM_WRAP0 + slice_num);            // Define o DREQ do PWM
 
+    // Configuração do PWM e DMA
     dma_channel_configure(
         dma_chan,
         &c,
@@ -50,23 +56,23 @@ int main(){
         false                          // não iniciar ainda
     );
 
-    dma_channel_set_irq0_enabled(dma_chan, true);
-    irq_set_exclusive_handler(DMA_IRQ_0, dma_handler);
-    irq_set_enabled(DMA_IRQ_0, true);
+    // Configuração da interrupção
+    dma_channel_set_irq0_enabled(dma_chan, true);       // Habilita a interrupção
+    irq_set_exclusive_handler(DMA_IRQ_0, dma_handler);  // Define a função de tratamento
+    irq_set_enabled(DMA_IRQ_0, true);                   // Habilita a interrupção
+    dma_channel_start(dma_chan);                        // Inicia o DMA
 
-    dma_channel_start(dma_chan);
+    while(true){
+        uint16_t compare_a = (uint16_t)(pwm_hw->slice[slice_num].cc & 0xFFFF);  // Obtem o valor de comparação
+        float duty_ratio = (float)compare_a / 65535.0f;                         // Calcula o duty ratio
+        int angle = (int)(duty_ratio * 180.0f);                                 // Converte para angulo
 
-    while (true) {
-        uint16_t compare_a = (uint16_t)(pwm_hw->slice[slice_num].cc & 0xFFFF);
-        float duty_ratio = (float)compare_a / 65535.0f;
-        int angle = (int)(duty_ratio * 180.0f);
-
-        char buf[32];
-        snprintf(buf, sizeof(buf), "Angulo: %3d", angle);
-
-        ssd1306_fill(&ssd, false);
-        ssd1306_draw_string(&ssd, buf, 24, 24);
-        ssd1306_send_data(&ssd);
+        char buf[32];                                                          // Buffer para armazenar a string
+        snprintf(buf, sizeof(buf), "Angulo: %3d", angle);                      // Formata a string
+        
+        ssd1306_fill(&ssd, false);                                        // Limpa o display
+        ssd1306_draw_string(&ssd, buf, 24, 24);                           // Escreve uma string no display
+        ssd1306_send_data(&ssd);                                          // Atualiza o display
 
         sleep_ms(100);
     }
